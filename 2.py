521e5db40ea89181772e751bb33b2b81f824e985
Aayush Mittal <aayush102003@gmail.com>
2021-12-07 23:43:23 +0530

    string formating

---
+++ 2.py
@@ -0,0 +1,11 @@
+num1 = 3.1654616518
+num2 = 145.154531313
+
+#earlier method 
+#print ('num1 is', num1, 'num2 is', num2)
+
+#formating
+#print('num1 is {0:.4} and num2 is {1:.6}'.format(num1,num2))
+
+#using f-string
+print(f'num 1 is{num1:.4f} and num 2 is {num2:.5f}')
