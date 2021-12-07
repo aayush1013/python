8255e2e3d7dc2116b8de7ab6d92082981141bd80
Aayush Mittal <aayush102003@gmail.com>
2021-12-07 23:22:14 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+name = input('Tell me your name:')
+age = input('and your age brother:')
+city = input ('And finally where do you live:')
+print(name,'you are', age,'years old','and you come from', city)
+
+
+
+radius = input('Enter the radius of the cicrle:')
+area = 3.14*int(radius)**2
+print ('the area of the circle is:', area)
