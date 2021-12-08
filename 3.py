7e6e4b50e8f58ba72b1b9be0076d60a9a47256d2
Aayush Mittal <aayush102003@gmail.com>
2021-12-08 22:45:48 +0530

    if-elif updated

--- 3.py
+++ 3.py
@@ -1,7 +1,12 @@
-Age = int(input('Enter the age:'))
+#Age = int(input('Enter the age:'))
 
-if Age<15:
-    print ('ohhh!!, you are quit young huhhhh...')
+#if Age<15:
+#   print ('ohhh!!, you are quit young huhhhh...')
+#else:
+#   print('i am bad at guessing ages :_:')         
+
+choice = input('Do you eat meat?(y/n):')        
+if choice == 'y':
+    print ('here is your meat menu...')
 else:
-    print('i am bad at guessing ages :_:')         
-           
+    print('here is your veg menu...')
