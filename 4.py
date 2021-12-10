66a5bb4e8916176738c607c3ce12475a65866f74
Aayush Mittal <aayush102003@gmail.com>
2021-12-10 12:00:08 +0530

    for loop updated

--- 4.py
+++ 4.py
@@ -1,4 +1,16 @@
 cric = ['sachin','sehwag','virat','pant','jadega','shami','bhuvi','bumrah']
 
+#for cri in cric:
+    #print(cri)
+    
+    #for cri in cric[1:3]:
+    #print(cri)
+    
+    
 for cri in cric:
-    print(cric)
+         if cri == 'sachin':
+             print(f'{cri} - red belt')
+             break
+         else:
+             print('jadega')
+         
