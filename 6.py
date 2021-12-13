d5a33697e9ff311fb9065b7ea18af8961604b516
Aayush Mittal <aayush102003@gmail.com>
2021-12-13 23:56:53 +0530

    range in python

---
+++ 6.py
@@ -0,0 +1,13 @@
+#for item in range(6):
+   # print (item)
+   
+#for item in range(2,6):
+    #print (item)
+    
+ice_cream = ['belgium choclate','pan','turkish','smoothy','pista'] 
+for n in range(len(ice_cream)):
+    print(n,ice_cream[n])
+    
+    
+   
+    
