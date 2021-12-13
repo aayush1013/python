48b1982c660d29988cedd55b9c4240183e7e871d
Aayush Mittal <aayush102003@gmail.com>
2021-12-14 00:04:29 +0530

    updated range in python

--- 6.py
+++ 6.py
@@ -4,10 +4,15 @@
 #for item in range(2,6):
     #print (item)
     
-ice_cream = ['belgium choclate','pan','turkish','smoothy','pista'] 
-for n in range(len(ice_cream)):
-    print(n,ice_cream[n])
+#for item in range(0,4,20):
+    #print (item)    
     
+ice_cream = ['belgium choclate','pan','turkish','smoothy','pista'] 
+#for n in range(len(ice_cream)):
+    #print(n,ice_cream[n])
+
+for n in range(len(ice_cream) -1, -1, -1):
+    print(n,ice_cream)
     
    
     
