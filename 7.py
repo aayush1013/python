a97fb6478111946e3e05a7590838dfe1adc3bb9f
Aayush Mittal <aayush102003@gmail.com>
2021-12-14 01:18:22 +0530

    function

---
+++ 7.py
@@ -0,0 +1,9 @@
+def lnmiit(name,season):
+    #print ("college bula lo guys")
+    
+#lnmiit()
+#lnmiit()
+ print (f'hey what is your {name}, do you like {season}, ohh great i love this season too..')
+ lnmiit('rohit','winters')
+ 
+ 
